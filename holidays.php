a10b30826c29303bf0115fa25acfea322740c320
Marc Antoni <ocanomark@yahoo.com>
2020-08-10 20:36:14 +0900

    Update holidays.php

--- holidays.php
+++ holidays.php
@@ -1,11 +1,19 @@
 <?php
-	$url = $_GET["url"];
+	$website = "https://calendarific.com/api/v2";
+	$apiKey = "***api key goes here***";
+	$params = $_GET["params"];
 	
-	if(isset($url)) {
-		//Use file_get_contents to GET the URL in question.
-		$contents = file_get_contents($url);
+	if (isset($params)) {
+		$url = "";
 		
-		//Print out the contents.
-		echo $contents;
+		if ($params["get"] == "countries") {
+			$url = $website . "/countries?api_key=" . $apiKey;
+		} else { //holidays
+			$url = $website . "/holidays?api_key=" . $apiKey . "&country=" .
+				$params["country"] . "&year=" . $params["year"];
+		}
+		
+		$response = file_get_contents($url);
+		echo $response;
 	}
 ?>
